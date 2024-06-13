355e83ff9011286d580bc1173672f6193b5d5e58
alexchoicy <alex123321choicy@gmail.com>
2024-06-14 04:23:35 +0800

    add more ignore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import dotenv from "dotenv";
 import { expressManager } from "./managers/expressManager";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./db/schemas"
+import { eq } from "drizzle-orm";
+import { alias } from "drizzle-orm/pg-core";
+import { parseFile } from "music-metadata";
 
 dotenv.config();
 
